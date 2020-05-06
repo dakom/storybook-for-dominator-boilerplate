bed1a2a98cd27c3fbe3993140a498f2aa128805b
David Komer <david.komer@gmail.com>
2020-05-07 00:31:54 +0300

    allow underscore gh_pages

--- src/utils/path.js
+++ src/utils/path.js
@@ -2,8 +2,7 @@ export const Path = (() => {
 
     const MEDIA_URL = process.env["NODE_ENV"] === "development" 
         ? 'http://localhost:4102'
-        : 'https://raw.githubusercontent.com/dakom/storybook-for-dominator-boilerplate/gh-pages/_static-media';
-        //: "https://dakom.github.io/storybook-for-dominator-boilerplate/_static-media";
+        : "https://dakom.github.io/storybook-for-dominator-boilerplate/_static-media";
 
     return {
         ui: path => `${MEDIA_URL}/app/ui/${path}`,
