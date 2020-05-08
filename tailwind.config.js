ad53a904ed6c75dd28b8ef69e0f37a779d0325af
David Komer <david.komer@gmail.com>
2020-05-08 13:42:02 +0300

    cleaner purge options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   purge: [
-    './src/html/**/*',
+    './src/html/**/*.js',
+    './src/html/**/*.ts',
+    './src/html/**/*.html',
+    './src/html/**/*.jsx',
+    './src/html/**/*.tsx',
   ],
   theme: {
     extend: {},
